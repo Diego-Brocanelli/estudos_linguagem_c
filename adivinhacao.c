e6e36f5cc8c381e26bc87d1d5aa6a8256e40649b
Diego Brocanelli <diegod2@msn.com>
2015-09-04 19:13:27 -0300

    Alteração referente a estrutura de repetição sendo implantado while, onde o processo de questionamento do sistema se dará até que o jogador acerte o número secreto. Atualização do README.ME com as novas orientações. Inclusão de .gitignore para não vercionar o arquivo executável.

--- adivinhacao.c
+++ adivinhacao.c
@@ -1,40 +1,49 @@
 #include <stdio.h>
+#define numerosecreto 42
 //primeiro código em C
 int main() {
+	
 	printf("************************************\n");
 	printf("* Bem-vindo ao jogo de Adivinhação *\n");
 	printf("************************************\n");	
 
-	//definido o número secreto
-	int numerosecreto;
-	numerosecreto = 42;
-
 	int chute;
-	int i;
-	for( i = 1; i <= 3; i++ ){
+	int tentativas =1;
 
+	while(1){
+
+		printf("Tentativa %d\n", tentativas);
 		printf("Qual é o seu chute? \n");
+
 		scanf("%d", &chute);
 		printf("Você chutou o número %d! \n", chute );
 
+		//analisa se o número é negativo
+		int numerosnegativos = chute < 0;
+		if(numerosnegativos){
+			printf("Você não pode chutar números negativos! \n");
+			continue;
+		}
+
 		int acertou = chute == numerosecreto;
+		int maior = chute > numerosecreto;
 
 		if(acertou){
 			
 			printf("Parabéns, você acertou! \n");
 			printf("Jogue de novo, você é um bom jogador! \n");
 
+			break;
+
+		}else if(maior){
+			printf("Seu chute foi maior que o número secreto! \n");
 		}else{
-
-			int maior = chute > numerosecreto;
-
-			if(maior){
-				printf("Seu chute foi maior que o número secreto! \n");	
-			}else{
-				printf("Seu chute foi menor do que o número secreto! \n");				
-			}
+			printf("Seu chute foi menor do que o número secreto! \n");							
 		}
+
+		tentativas++;
 	}
 
 	printf("Fimm de jogo! \n");
+	printf("Você acertou em %d tentativas! \n", tentativas);
 }
